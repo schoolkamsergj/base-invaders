b2c397759026ba7ac9ce6a8c27ce42e4200a06be
Penzlik <schoolkam@gmail.com>
2026-02-02 18:22:11 +0200

    removed the red Reset button

--- ui.js
+++ ui.js
@@ -255,36 +255,6 @@ class UI {
             }
             this.scene.togglePause();
         });
-
-        // TEMPORARY: Reset check-in button for testing
-        this.resetCheckInBtn = this.scene.add.rectangle(
-            this.scene.scale.width - 60,
-            90,
-            50, 30,
-            0xff0000, // Red
-            0.8
-        );
-        this.resetCheckInBtn.setScrollFactor(0);
-        this.resetCheckInBtn.setDepth(100);
-        this.resetCheckInBtn.setInteractive({ useHandCursor: true });
-
-        this.resetCheckInBtnText = this.scene.add.text(
-            this.scene.scale.width - 60,
-            90,
-            '🔄 Reset',
-            { fontSize: '12px', align: 'center' }
-        );
-        this.resetCheckInBtnText.setOrigin(0.5);
-        this.resetCheckInBtnText.setScrollFactor(0);
-        this.resetCheckInBtnText.setDepth(101);
-
-        this.resetCheckInBtn.on('pointerdown', () => {
-            localStorage.removeItem(this.getLastCheckInKey());
-            localStorage.removeItem(this.getStreakKey());
-            alert('Check-in reset!');
-            this.updateCheckInButtonState();
-            if (this.scene.playSound) this.scene.playSound('click');
-        });
     }
 
     getLayoutMetrics(width = this.scene.scale.width, height = this.scene.scale.height) {
@@ -400,14 +370,6 @@ class UI {
         this.pauseBtn.setSize(layout.pauseWidth, layout.pauseHeight);
         this.pauseBtnText.setPosition(layout.pauseX, layout.pauseY);
         this.pauseBtnText.setFontSize(`${layout.pauseFont}px`);
-
-        // TEMPORARY: Reset check-in button (below pause)
-        if (this.resetCheckInBtn) {
-            const resetX = layout.width - layout.margin - 30;
-            const resetY = layout.pauseY + layout.pauseHeight + 15;
-            this.resetCheckInBtn.setPosition(resetX, resetY);
-            this.resetCheckInBtnText.setPosition(resetX, resetY);
-        }
 
         // Shop button (bottom left, above health bar) - use pre-calculated metrics
         this.shopBtn.setPosition(layout.shopX, layout.shopY);
